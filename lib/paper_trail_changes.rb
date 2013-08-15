b474a139345aacfeedda0aa00262cd98faf0e191
kaspernj <k@spernj.org>
2013-08-15 12:55:00 +0200

    Made the gem actually work.

--- lib/paper_trail_changes.rb
+++ lib/paper_trail_changes.rb
@@ -1,6 +1,98 @@
+require "string-cases"
+
 class PaperTrailChanges
-  def last_version(model)
+  def self.last_version(model)
     version = Version.where(:item_type => model.class.name, :item_id => model.id, :event => :update).order(:id).reverse_order.first
     return version
+  end
+  
+  CHANGES_SINCE_VERSION_VALID_ARGS = [:version_id, :version_at, :model, :attributes]
+  def self.changes_since_version(args)
+    args.each do |key, val|
+      raise "Invalid argument: '#{key}'." unless CHANGES_SINCE_VERSION_VALID_ARGS.include?(key)
+    end
+    
+    attributes, model = args[:attributes], args[:model]
+    
+    column_types = PaperTrailChanges.column_types_from_class(model.class)
+    
+    if args[:version_id]
+      version_obj = Version.find(args[:version_id]) rescue nil
+    elsif args[:version_at]
+      version_model = model.version_at(args[:version_at])
+    end
+    
+    version_model = model if version_obj.nil? && version_model.nil?
+    
+    if version_obj
+      version_hash = Psych.load(version_obj.object).stringify_keys
+    elsif version_model
+      version_hash = {}
+      version_model.attributes.each do |key, val|
+        version_hash[key.to_s] = version_model.__send__("#{key}_before_type_cast")
+      end
+    else
+      raise "Dont know what to do?"
+    end
+    
+    changes_since = PaperTrailChanges.changes_hash(
+      :attributes => attributes,
+      :version_hash => version_hash,
+      :model => model,
+      :column_types => column_types
+    )
+    
+    return changes_since
+  end
+  
+  def self.column_types_from_class(class_obj)
+    column_types = {}
+    class_obj.columns_hash.each do |name, col|
+      column_types[name] = col.type
+    end
+    
+    return column_types
+  end
+  
+  CHANGES_HASH_VALID_ARGS = [:attributes, :version_hash, :model, :column_types]
+  def self.changes_hash(args)
+    args.each do |key, val|
+      raise "Invalid argument: '#{key}'." unless CHANGES_HASH_VALID_ARGS.include?(key)
+    end
+    
+    attributes, version_hash, model, column_types = args[:attributes], args[:version_hash], args[:model], args[:column_types]
+    changes_since = {}
+    
+    attributes.each do |key, val|
+      key_s = key.to_s
+      
+      if match = key_s.match(/^(.+)_attributes$/)
+        # Nested model. Since paper-trail doesn't keep track of this just pass it through.
+        changes_since[key] = val
+      elsif version_hash.key?(key_s)
+        last_val = version_hash[key_s]
+        changed = false
+        type = column_types[key_s]
+        
+        if type == :string || type == :date || type == :text || type == :datetime
+          changed = true if last_val.to_s != val.to_s
+        elsif type == :integer
+          changed = true if last_val.to_i != val.to_i
+          changed = true if last_val == nil && val
+        elsif type == :boolean
+          bool_i = last_val ? 1 : 0
+          changed = true if bool_i != val.to_i
+          changed = true if last_val == nil && val
+        else
+          raise "Unknown type: '#{type}'."
+        end
+        
+        changes_since[key_s] = val if changed
+      else
+        changes_since[key_s] = val
+      end
+    end
+    
+    return changes_since
   end
 end
--- spec/paper_trail_changes_spec.rb
+++ spec/paper_trail_changes_spec.rb
@@ -1,5 +1,53 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
+class Submodel
+  def columns_hash
+    
+  end
+end
+
 describe "PaperTrailChanges" do
+  it "should handle booleans" do
+    changes = PaperTrailChanges.changes_hash(
+      :attributes => {"works" => "1", "fails" => 0, "doesnt_fail" => "1"},
+      :version_hash => {"works" => true, "fails" => false, "doesnt_fail" => false},
+      :column_types => {"works" => :boolean, "fails" => :boolean, "doesnt_fail" => :boolean}
+    )
+    
+    changes.length.should eql(1)
+    changes.keys.first.should eql("doesnt_fail")
+    changes.values.first.should eql("1")
+  end
   
+  it "should handle integers" do
+    changes = PaperTrailChanges.changes_hash(
+      :attributes => {"number1" => "1", "number2" => "2", "number3" => "3"},
+      :version_hash => {"number1" => 1, "number2" => 2, "number3" => 33},
+      :column_types => {"number1" => :integer, "number2" => :integer, "number3" => :integer} 
+    )
+    
+    changes.length.should eql(1)
+    changes.keys.first.should eql("number3")
+    changes.values.first.should eql("3")
+  end
+  
+  it "should handle strings" do
+    changes = PaperTrailChanges.changes_hash(
+      :attributes => {"str1" => "123", "str2" => "234", "str3" => 3},
+      :version_hash => {"str1" => "1234", "str2" => "234", "str3" => 3},
+      :column_types => {"str1" => :string, "str2" => :text, "str3" => :string}
+    )
+    
+    changes.length.should eql(1)
+    changes.keys.first.should eql("str1")
+    changes.values.first.should eql("123")
+  end
+  
+  it "should handle nested attributes" do
+    changes = PaperTrailChanges.changes_hash(
+      :attributes => {"submodel_attributes" => {"test1" => "test1", "test2" => 2, "test3" => "changed"}},
+      :version_hash => {"submodel" => {"test1" => "test1", "test2" => 2, "test3" => "test3"}},
+      :column_types => {}
+    )
+  end
 end
